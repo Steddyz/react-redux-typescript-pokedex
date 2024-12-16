2c18c89cca68145fb7565510bbed85b21653c310
Steddy <qwesteddyoff@gmail.com>
2024-12-16 20:39:18 +0300

    switch Pokedex Page

--- src/pages/PokedexPage/PokedexPage.tsx
+++ src/pages/PokedexPage/PokedexPage.tsx
@@ -4,16 +4,21 @@ import cl from "./PokedexPage.module.css";
 import Form from "../../components/Form/Form";
 import PokemonCard from "../../components/PokemonCard/PokemonCard";
 import Pagination from "../../components/Pagination/Pagination";
+import { useGetPokemonsQuery } from "../../services/pokemonService";
 
 const PokedexPage: FC = () => {
   const [page, setPage] = useState<number>(0);
   const [search, setSearch] = useState<string>("");
 
+  const { data, isLoading, isError } = useGetPokemonsQuery({ page, search });
+
   return (
     <div className={cl.pokedex}>
       <Form onSearch={setSearch} />
       <Pagination onPageChange={setPage} currentPage={page} />
-      <PokemonCard page={page} search={search} />
+      {isLoading && <div>Загрузка...</div>}
+      {isError && <div>Произошла ошибка. Попробуйте позже</div>}
+      {data && <PokemonCard pokemons={data} />}
     </div>
   );
 };
--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,9 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import axios from "axios";
 
-type Pokemon = {
+export type Pokemon = {
   name: string;
   sprites: { front_default: string };
 };
+
+interface PokemonResponse {
+  results: { url: string; name: string }[];
+  count: number;
+}
 
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
@@ -12,7 +18,26 @@ export const pokemonApi = createApi({
     getRandomPokemon: builder.query<Pokemon, number>({
       query: (id) => `pokemon/${id}`,
     }),
+    getPokemons: builder.query<Pokemon[], { page: number; search: string }>({
+      query: ({ page, search }) => {
+        const limit = 20;
+        const offset = page * limit;
+
+        return search
+          ? `pokemon?limit=${limit}&offset=${offset}&search=${search}`
+          : `pokemon?limit=${limit}&offset=${offset}`;
+      },
+      transformResponse: async (response: PokemonResponse) => {
+        const pokemonData = await Promise.all(
+          response.results.map(async (pok) => {
+            const pokResponse = await axios.get(pok.url);
+            return await pokResponse.data;
+          })
+        );
+        return pokemonData;
+      },
+    }),
   }),
 });
 
-export const { useGetRandomPokemonQuery } = pokemonApi;
+export const { useGetRandomPokemonQuery, useGetPokemonsQuery } = pokemonApi;
